32d50aac213bc5eb46aaab8f82e07e4c4020d691
Mohammed Abdellatief <mabdellatief@gmail.com>
2018-05-19 03:48:37 +0200

    my first project ERD

---
+++ js/erd.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    // do not apply animation if on mobile (for better performance)
+    if( !/Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)  &&  $(window).width() >= 640) {
+
+
+    }
+
+
+
+
+
+});
+
